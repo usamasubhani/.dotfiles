5370563a834072b280baa07b71ad8fe3774e71af
Usama Subhani <myaccuounts@protonmail.ch>
2018-07-02 15:51:28 +0500

    Migrated from github

---
+++ .config/i3/lock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png -paint 3 -modulate 30 /tmp/screen.png
+[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+moc -P
+i3lock -e -i /tmp/screen.png
