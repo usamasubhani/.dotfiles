23f96ca66b8d59326b2a906e6ba16d48fa4c1212
Usama Subhani <myaccuounts@protonmail.ch>
2018-07-29 21:51:06 +0500

    Script for dual external monitors

--- Scripts/monitors.sh
+++ Scripts/monitors.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-x=$(echo -e "VGA-Right\nVGA-Left\nLaptop\nManual" | dmenu -b -i -p "Select Mode")
+x=$(echo -e "VGA-Right\nVGA-Left\nLaptop\nManual\nDual-External" | dmenu -b -i -p "Select Mode")
 
 case $x in
 	VGA-Right) xrandr --output VGA1 --auto --right-of LVDS1  ;;
 	VGA-Left)  xrandr --output VGA1 --auto --left-of LVDS1 ;;
 	Laptop) xrandr --output VGA1 --off ;;
+	Dual-External) xrandr --output LVDS1 --off --output VGA1 --auto --output DP1 --auto --right-of VGA1 && polybar external ;;
 	Manual) arandr;;
 
 
