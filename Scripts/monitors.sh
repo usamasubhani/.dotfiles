1b82150005df86ec9ed39239b452873a0e2cb5fc
Usama Subhani <myaccuounts@protonmail.com>
2018-12-28 22:48:04 +0500

    Rebirth... New setup

--- Scripts/monitors.sh
+++ Scripts/monitors.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-x=$(echo -e "VGA-Right\nVGA-Left\nLaptop\nManual\nDual-External" | dmenu -b -i -p "Select Mode")
+x=$(echo -e "Dock\nVGA-Right\nVGA-Left\nLaptop\nManual\nDual-External" | dmenu -b -i -p "Select Mode")
 
 case $x in
 	VGA-Right) xrandr --output VGA1 --auto --right-of LVDS1  ;;
 	VGA-Left)  xrandr --output VGA1 --auto --left-of LVDS1 ;;
 	Laptop) xrandr --output VGA1 --off ;;
-	Dual-External) xrandr --output LVDS1 --off --output VGA1 --auto --output DP1 --auto --right-of VGA1 && killall polybar && polybar external ;;
+	Dock) xrandr --output LVDS-1 --off --output DP-1 --mode 1280x1024 --output VGA-1 --auto --left-of DP-1 & polybar_launch ;;
+	Dual-External) xrandr --output LVDS-1 --off --output DP-1 --auto --output VGA-1 --auto --left-of DP-1 && killall polybar && polybar external ;;
 	Manual) arandr;;
 
 
